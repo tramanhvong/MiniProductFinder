84ea74363d13d981e2b55616ac2479167f0773d3
Tram Anh Vong <vongta21@my.yorku.ca>
2025-11-06 12:32:45 +0700

    fixing fetch api

--- script.js
+++ script.js
@@ -2,20 +2,56 @@ const allSearch = "https://dummyjson.com/products?limit=10";
 const specSearch = "https://dummyjson.com/products/search?q=";
 
 
-async function search() {
-    try {
-        const keyword = document.getElementById("keyword").value;
-        let url = specSearch + keyword;
-        alert(url);
+// function fetchAll() {
+//         fetch(allSearch)
+//         .then((response) => response.json())
+//         .then((data => {
+//             alert(data);
+//             const resultGrids = document.getElementById("resultDisplay");
+//             for (const product of data.products) {
+//                 const item = document.createElement("div")
+//                 item.innerHTML = `<p>${product.category}\n$${product.price}\n${product.rating}\n${product.availabilityStatus}</p>`;
 
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-        
-        const result = await response.json();
-        alert(result);
-    } catch (error) {
-        console.error(error.message);
+//                 resultGrids.appendChild(item);
+//             }
+//         }))
+//         .catch(console.error);
+// }
+
+async function fetchAll() {
+  try {
+    const response = await fetch(allSearch);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+
+    const result = response.json();
+    const data = JSON.parse(result);
+    for (var product of data.products) {
+            const item = document.createElement("div")
+            item.innerHTML = `<p>${product.category}\n$${product.price}\n${product.rating}\n${product.availabilityStatus}</p>`;
+
+            resultGrids.appendChild(item);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function search() {
+        const keyword = document.getElementById("keyword").value;
+
+        fetch(specSearch +`${keyword}`)
+            .then((response) => response.json())
+            .then((data => {
+                const resultGrids = document.getElementById("resultDisplay");
+                for (const product of data.products) {
+                    const item = document.createElement("li");
+                    item.appendChild(document.createElement("strong")).textContent = product.title;
+                    item.appendChild(document.createElement("p")).innerHTML = `${product.category}\n$${product.price}\n${product.rating}\n${product.availabilityStatus}`;
+
+                    resultGrids.appendChild(item);
+                }
+            }))
+            .catch(console.error);
 }
