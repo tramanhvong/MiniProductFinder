e796787ca21340a7f005f574301110076ff2b1f1
Tram Anh Vong <vongta21@my.yorku.ca>
2025-11-06 11:40:18 +0700

    added fetch api, fixed filter

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const api = "https://dummyjson.com/products?limit=10"
+const allSearch = "https://dummyjson.com/products?limit=10";
+const specSearch = "https://dummyjson.com/products/search?q=";
 
-function search() {
-    alert("change")
+
+async function search() {
+    try {
+        const keyword = document.getElementById("keyword").value;
+        let url = specSearch + keyword;
+        alert(url);
+
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const result = await response.json();
+        alert(result);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
